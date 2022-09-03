37fba4d24ec198a12e380a89b48c0bb3c7b0a7ab
Tahmina Ria <tahminariya459@gmail.com>
2022-09-03 19:41:19 +0600

    sorting

--- js/app.js
+++ js/app.js
@@ -50,41 +50,53 @@ const displayNews = async(data) =>{
     }
     const newsContainer = document.getElementById('news-container');
     newsContainer.textContent ='';
-    
-    for(const news of data){
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div class="card lg:card-side bg-base-100 shadow-xl rounded-none p-4">
-            <figure><img class="w-64 h-full rounded-md" src="${news.thumbnail_url}" alt="Album"></figure>
-            <div class="card-body">
-                <h2 class="card-title">${news.title? news.title : 'No Information Found'}</h2>
-                <p>${news.details.length > 200 ? news.details.slice(0,200) + '...' : news.details }</p>
-                <div class="card-actions justify-between mt-3 items-center">
-                    <div class="flex items-center"> 
-                        <img class="h-10 w-10 rounded-full" src="${news.author.img}">
-                        <div class="mx-2"> 
-                            <h5 class=""> ${news.author.name? news.author.name : 'No Information Found'} </h5>
-                            <h5 class="text-sm text-gray-400"> ${news.author.published_date? news.author.published_date : 'No Information Found'} </h5>
 
+    // function minView(){
+    //     data.sort((a,b) => a.total_view - b.total_view);
+    //     return data
+
+    // }
+    // function maxView(){
+    //     data.sort((a,b) => b.total_view - a.total_view);
+    //     return data;
+    // }
+
+    for(const news of data){
+            const div = document.createElement('div');
+            div.innerHTML = `
+            <div class="card lg:card-side bg-base-100 shadow-xl rounded-none p-4">
+                <figure><img class="w-64 h-full rounded-md" src="${news.thumbnail_url}" alt="Album"></figure>
+                <div class="card-body">
+                    <h2 class="card-title">${news.title? news.title : 'No Information Found'}</h2>
+                    <p>${news.details.length > 200 ? news.details.slice(0,200) + '...' : news.details }</p>
+                    <div class="card-actions justify-between mt-3 items-center">
+                        <div class="flex items-center"> 
+                            <img class="h-10 w-10 rounded-full" src="${news.author.img}">
+                            <div class="mx-2"> 
+                                <h5 class=""> ${news.author.name? news.author.name : 'No Information Found'} </h5>
+                                <h5 class="text-sm text-gray-400"> ${news.author.published_date? news.author.published_date : 'No Information Found'} </h5>
+    
+                            </div>
                         </div>
+                        <div> 
+                        <p> <i class="fa-regular fa-eye"></i> ${news.total_view} </p>
+                        </div>
+                        <div> 
+                        <p> <i class="fa-solid fa-star"></i> ${news.rating.number} </p>
+                        </div>
+                        
+                        <label onclick="loadNewsDetails('${news._id}')" for="my-modal-6" class="btn border-none bg-transparent hover:bg-indigo-200 modal-button">
+                        <i class="text-2xl text-indigo-700 fa-solid fa-arrow-right-long"></i>
+                        </label>
                     </div>
-                    <div> 
-                    <p> <i class="fa-regular fa-eye"></i> ${news.total_view} </p>
-                    </div>
-                    <div> 
-                    <p> <i class="fa-solid fa-star"></i> ${news.rating.number} </p>
-                    </div>
-                    
-                    <label onclick="loadNewsDetails('${news._id}')" for="my-modal-6" class="btn border-none bg-transparent hover:bg-indigo-200 modal-button">
-                    <i class="text-2xl text-indigo-700 fa-solid fa-arrow-right-long"></i>
-                    </label>
                 </div>
             </div>
-        </div>
-        `
-        newsContainer.appendChild(div);
-
+            `
+            newsContainer.appendChild(div);
     }
+
+    
+    
 
 }
 
