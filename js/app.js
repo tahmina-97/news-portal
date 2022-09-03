9e95a79682c2783c1aeed0ff468e664b57562c76
Tahmina Ria <tahminariya459@gmail.com>
2022-09-03 21:35:18 +0600

    correction

--- js/app.js
+++ js/app.js
@@ -40,8 +40,7 @@ const loadCategoryNews =  async(categoryId) =>{
 }
 
 const displayNews = async(data) =>{
-    console.log(data);
-    const categoryData = await loadAllNews();
+    // const categoryData = await loadAllNews();
     const notFoundMsg = document.getElementById('no-news-found');
     const newsFound = document.getElementById('news-found');
     newsFound.innerHTML=`${data.length} items found for category`;
@@ -64,6 +63,10 @@ const displayNews = async(data) =>{
     //     return data;
     // }
 
+
+
+    data.sort((a,b) => b.total_view - a.total_view);
+
     for(const news of data){
             const div = document.createElement('div');
             div.innerHTML = `
@@ -71,21 +74,21 @@ const displayNews = async(data) =>{
                 <figure><img class="w-64 h-full rounded-md" src="${news.thumbnail_url}" alt="Album"></figure>
                 <div class="card-body">
                     <h2 class="card-title">${news.title? news.title : 'No Information Found'}</h2>
-                    <p>${news.details.length > 200 ? news.details.slice(0,200) + '...' : news.details }</p>
+                    <p>${news.details.length > 280 ? news.details.slice(0,280) + '...' : news.details }</p>
                     <div class="card-actions justify-between mt-3 items-center">
                         <div class="flex items-center"> 
                             <img class="h-10 w-10 rounded-full" src="${news.author.img}">
                             <div class="mx-2"> 
-                                <h5 class=""> ${news.author.name? news.author.name : 'No Information Found'} </h5>
-                                <h5 class="text-sm text-gray-400"> ${news.author.published_date? news.author.published_date : 'No Information Found'} </h5>
+                                <h5 class=""> ${news.author?.name? news.author.name : 'No Information Found'} </h5>
+                                <h5 class="text-sm text-gray-400"> ${news.author?.published_date? news.author.published_date : 'No Information Found'} </h5>
     
                             </div>
                         </div>
                         <div> 
-                        <p> <i class="fa-regular fa-eye"></i> ${news.total_view} </p>
+                        <p> <i class="fa-regular fa-eye"></i> ${news.total_view? news.total_view : 'No Info Found' } </p>
                         </div>
                         <div> 
-                        <p> <i class="fa-solid fa-star"></i> ${news.rating.number} </p>
+                        <p> <i class="fa-solid fa-star"></i> ${news.rating?.number? news.rating?.number :  'No Info Found' } </p>
                         </div>
                         
                         <label onclick="loadNewsDetails('${news._id}')" for="my-modal-6" class="btn border-none bg-transparent hover:bg-indigo-200 modal-button">
@@ -99,9 +102,6 @@ const displayNews = async(data) =>{
     }
 
     toggleSpinner(false);
-
-    
-    
 
 }
 
@@ -120,7 +120,7 @@ const loadNewsDetails = async(newsId)=>{
 }
 
 const displayNewsDetails = async(news)=>{
-    console.log(news);
+    // console.log(news);
     const modalBody= document.getElementById('modal-body');
     modalBody.innerHTML=`
         <h3 class="font-bold text-lg pb-4">${news.title? news.title : 'No Title Found'}</h3>
